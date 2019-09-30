fbba91e733510a835406822d399ec44d8b5ed4ce
Luana-GD <l.trannguyen@rubika-edu.com>
2019-09-30 13:43:24 +0200

    prière pour que ça marche

---
+++ R_L_test/Assets/LTN_Scripts/LvlGeneration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlGeneration : MonoBehaviour
+{
+    public Transform[] roomSpawner;
+    public GameObject[] rooms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randRoomSpawn = Random.Range(0, roomSpawner.Length);
+        transform.position = roomSpawner[randRoomSpawn].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ R_L_test/Assets/LTN_Scripts/SpawnTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTile : MonoBehaviour
+{
+
+    public GameObject[] objects; //Y insérer des GameObject. La longueur augmente avec les ajouts.
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, objects.Length); //choix d'un objet random de la liste 
+        Instantiate(objects[rand], transform.position, Quaternion.identity); //instancier l'objet choisi au pif, à la position de l'empty, sans rotation
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
