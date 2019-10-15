41d408f31e7006354b834d71ccf24e2a06641ce6
Luana-GD <l.trannguyen@rubika-edu.com>
2019-10-15 16:09:37 +0200

    A random room can spawn at one place. Just that.

--- R_L_test/Assets/LTN_Scripts/LvlGeneration.cs
+++ R_L_test/Assets/LTN_Scripts/LvlGeneration.cs
@@ -7,17 +7,42 @@ public class LvlGeneration : MonoBehaviour
     public Transform[] roomSpawner;
     public GameObject[] rooms;
 
+    public int direction;
+    public float moveAmount;
+
     // Start is called before the first frame update
     void Start()
     {
         int randRoomSpawn = Random.Range(0, roomSpawner.Length);
         transform.position = roomSpawner[randRoomSpawn].position;
         int randRoomType = Random.Range(0, rooms.Length);
+        Instantiate(rooms[randRoomType], transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 6);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void MovPos()
+    {
+        if (direction == 1 || direction == 2) //move right
+        {
+            Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+            transform.position = newPos;
+        }
+        else if (direction == 3 || direction == 4) //move left
+        {
+            Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+            transform.position = newPos;
+        }
+
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+        direction = Random.Range(1, 5);
     }
 }
