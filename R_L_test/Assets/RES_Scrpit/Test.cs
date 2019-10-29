31d4bcd8abb7a65dd13cc34a011349bd422ecafe
Rémi Sécher <screams_shots@yahoo.fr>
2019-10-29 16:33:46 +0100

    Déplacement v1 (velocity)
    Test de la velocité hors d'un prefabs

---
+++ R_L_test/Assets/RES_Scrpit/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rgb;
+    public float horizontalMoove;
+    public float verticalMoove;
+    public Vector3 moove;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMoove = Input.GetAxis("Horizontal");
+        verticalMoove = Input.GetAxis("Vertical");
+        moove = new Vector3 (horizontalMoove,verticalMoove,0);
+
+        rgb.velocity = moove * speed * Time.fixedDeltaTime;
+        
+    }
+}
